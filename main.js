20709c8b33c27097cc36aab13e0d5a81058b4e79
BenGallardBridger <ben.gallard.bridger@gmail.com>
2021-11-09 16:42:19 +0000

    Attempt for responsive tabs

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+(function ($) {
+  $('.nav-tabs').responsiveTabs();
+})(jQuery);
+
+@media (min-width:768px) {
+  .responsive-tabs .dropdown-arrow {
+    display:none;
+  }
+}
+@mediascreen and (max-width:767px) {
+  /* styles here */
+}
