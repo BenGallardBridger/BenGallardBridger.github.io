99336c640a92dc21b661209485403439d9d67caf
BenGallardBridger <ben.gallard.bridger@gmail.com>
2021-11-09 16:43:57 +0000

    Reverted Back

--- main.js
+++ main.js
@@ -5,15 +5,3 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
-(function ($) {
-  $('.nav-tabs').responsiveTabs();
-})(jQuery);
-
-@media (min-width:768px) {
-  .responsive-tabs .dropdown-arrow {
-    display:none;
-  }
-}
-@mediascreen and (max-width:767px) {
-  /* styles here */
-}
